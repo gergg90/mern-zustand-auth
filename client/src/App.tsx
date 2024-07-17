78a19789797eb2bf4a30cd65a241d32387314ac1
Germain <germain.gg90@gmail.com>
2024-07-17 08:05:25 -0400

    login page client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,31 @@
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="container mx-auto">
-        <body>
-          <header>
+    <div className="container mx-auto">
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="grid grid-cols-2">
+          {" "}
+          <header className="col-span-2">
             <ModeToggle />
           </header>
-          <main>
-            <div>Hello World</div>
-            <Button>Click</Button>
+          <main className="col-span-2">
+            <div className="flex flex-col items-center justify-center">
+              <LoginPage />
+              <Button>Click</Button>
+            </div>
           </main>
-          <footer>
-            <p>Footer</p>
+          <footer className="col-span-2">
+            <div className="flex items-center justify-center ">
+              <p>Footer</p>
+            </div>
           </footer>
-        </body>
-      </div>
-    </ThemeProvider>
+        </div>
+      </ThemeProvider>
+    </div>
   );
 }
 

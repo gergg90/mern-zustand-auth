fd8cf19ef0d19587d053962f2d1ba1c50f23e2f1
Germain <germain.gg90@gmail.com>
2024-07-17 20:23:39 -0400

    defining form

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,4 +1,29 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z.string({
+    message: "Password is required",
+  }),
+});
+
 function LoginPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return <div>LoginPage</div>;
 }
 

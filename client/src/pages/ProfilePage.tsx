53f7212da9104ac3ffafda4e8cffe694729c175f
Germain <germain.gg90@gmail.com>
2024-07-21 17:47:03 -0400

    corrections

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
   const logOut = useAuthStore((state) => state.logOut);
+  const profile = useAuthStore((state) => state.profile);
+
+  console.log(profile);
+
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -14,6 +18,9 @@ function ProfilePage() {
   return (
     <div>
       <h1>Profile Page</h1>
+
+      <p>{profile.email}</p>
+
       <Button variant="destructive" onClick={handleClick}>
         LogOut
       </Button>
